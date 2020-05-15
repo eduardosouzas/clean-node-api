5b4b2c0cc54094825bc9eddf339f62acd49545b4
Edu Souza <eduardomail@gmail.com>
2020-05-14 21:23:26 -0300

    chore: add jest configs and scripts

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,5 +1,6 @@
 import { HttpResponse } from '../protocols/http'
 import { ServerError } from '../errors'
+
 export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: error
